d8d90d3b51f03f1d5ca3c04b3ef7b43474e83bd4
SlawCzech <slaw.czech@gmail.com>
2023-06-17 22:24:04 +0200

    updates to view and template

--- backend/exchange_rates/views.py
+++ backend/exchange_rates/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import FormView
-
+from django.core import serializers
 from . import forms, models
 
 
 class ExchangeRateFormView(FormView):
     template_name = 'exchange_rates/exchange_rate_view.html'
     form_class = forms.ExchangeRateForm
+    success_url = "/"
 
     def get_form(self, form_class=None):
         currencies = models.CurrencyName.objects.all()
@@ -14,3 +15,29 @@ class ExchangeRateFormView(FormView):
         form.fields["currency"].choices = [(currency.code, currency.name.capitalize()) for currency in currencies]
 
         return form
+
+    def form_valid(self, form):
+        start_date = form.cleaned_data.get("start_date")
+        end_date = form.cleaned_data.get("end_date")
+        currencies = form.cleaned_data.get("currency")
+
+        dates = models.CurrencyDate.objects.filter(date__range=(start_date, end_date))
+
+        currency_data = models.CurrencyValue.objects.filter(currency_date__date__range=(start_date, end_date),
+                                                            currency_name__code__in=currencies)
+
+        data = {
+            "labels": [date.date.strftime("%Y-%m-%d") for date in dates],
+            "datasets": [{
+                "label": currency,
+                "data": [
+                    float(value.exchange_rate) for value in currency_data if value.currency_name.code == currency
+                ]
+            } for currency in currencies]
+        }
+
+        context = self.get_context_data()
+        context.update({"currency_data": currency_data})
+        context.update({"data": data})
+
+        return self.render_to_response(context)
